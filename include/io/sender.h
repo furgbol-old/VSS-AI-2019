435ec3849853c8938412bbbdd21ffd824cc68a8b
Miguel Martins dos Santos <miguelmartinsdossantos@gmail.com>
2019-10-16 16:35:47 -0300

    Adding furgbol-core submodule

--- include/io/sender.h
+++ include/io/sender.h
@@ -5,9 +5,11 @@
 #define SENDER_H
 
 
-#include "Communications/CommandSender.h"
+#include "labels/labels.h"
 
-#include <boost/asio.hpp>
+#include "Communications/CommandSender.h"
+#include "furgbol-core/io/serial_writer.h"
+
 #include <chrono>
 #include <cinttypes>
 #include <mutex>
@@ -20,57 +22,39 @@ namespace io {
 
 class Sender {
     private:
-        boost::asio::streambuf buf_;
-        boost::asio::io_service io_service_;
-        boost::asio::streambuf::const_buffers_type buffer_;
-        boost::asio::serial_port port_;
-
-        vss::CommandSender *command_sender_;
-        vss::Command command_;
-
-        int mode_;
-        int team_color_;
-        int which_queue_;
-
         bool *running_;
         bool *changed_;
 
-        bool *gk_is_running_;
-        bool *cb_is_running_;
-        bool *st_is_running_;
-
-        std::string port_name_;
-        int frequency_;
-        float period_;
-        std::chrono::duration<float> sending_frequency_;
-
-        std::queue<std::vector<uint8_t>> *gk_sending_queue_;
-        std::queue<std::vector<uint8_t>> *cb_sending_queue_;
-        std::queue<std::vector<uint8_t>> *st_sending_queue_;
-
         std::mutex mutex_;
 
-        float linear_velocity_;
-        float angular_velocity_;
-        int linear_direction_;
-        int angular_direction_;
+        int execution_mode_;
+        int team_color_;
 
-        float velocity_right_;
-        float velocity_left_;
+        vss::CommandSender *command_sender_;
+        furgbol::io::SerialWriter *serial_writer_;
+
+        std::string port_name_;
+        int package_size_;
+
+        std::queue<std::vector<uint8_t>> gk_sending_queue_;
+        std::queue<std::vector<uint8_t>> cb_sending_queue_;
+        std::queue<std::vector<uint8_t>> st_sending_queue_;
+        int which_queue_;
+
+        std::vector<uint8_t> buffer_to_send_;
 
         void configure();
         void printConfigurations();
 
+        void send();
+
+        void calculateWheelsVelocity();
+
         void exec();
         void end();
-
-        void send(std::vector<unsigned char> buffer);
-
-        void calculateVelocity();
-        void errorCorrector();
         
     public:
-        Sender(int execution_mode, int team_color_, bool *running, bool *changed, bool *gk_is_running, bool *cb_is_running, bool *st_is_running, std::queue<std::vector<uint8_t>> *gk_sending_queue, std::queue<std::vector<uint8_t>> *cb_sending_queue, std::queue<std::vector<uint8_t>> *st_sending_queue);
+        Sender(bool *running, bool *changed, int execution_mode, int team_color_, std::queue<std::vector<uint8_t>> &gk_sending_queue, std::queue<std::vector<uint8_t>> &cb_sending_queue, std::queue<std::vector<uint8_t>> &st_sending_queue);
         ~Sender();
 
         void init();
--- src/io/sender.cc
+++ src/io/sender.cc
@@ -1,7 +1,6 @@
 // ® Copyright FURGBot 2019
 
 
-#include "labels/labels.h"
 #include "io/sender.h"
 
 #include "json.hpp"
@@ -13,54 +12,59 @@
 namespace vss_furgbol {
 namespace io {
 
-Sender::Sender(int execution_mode, int team_color, bool *running, bool *changed, bool *gk_is_running, bool *cb_is_running, bool *st_is_running, std::queue<std::vector<uint8_t>> *gk_sending_queue, std::queue<std::vector<uint8_t>> *cb_sending_queue, std::queue<std::vector<uint8_t>> *st_sending_queue) :
+Sender::Sender(bool *running, bool *changed, int execution_mode, int team_color, std::queue<std::vector<uint8_t>> &gk_sending_queue, std::queue<std::vector<uint8_t>> &cb_sending_queue, std::queue<std::vector<uint8_t>> &st_sending_queue) :
+    running_(running), changed_(changed), execution_mode_(execution_mode), team_color_(team_color),
     gk_sending_queue_(gk_sending_queue), cb_sending_queue_(cb_sending_queue), st_sending_queue_(st_sending_queue),
-    mode_(execution_mode), io_service_(), port_(io_service_), buffer_(buf_.data()), running_(running),
-    changed_(changed), which_queue_(GK), gk_is_running_(gk_is_running), cb_is_running_(cb_is_running),
-    st_is_running_(st_is_running) {}
+    which_queue_(GK) {}
 
 Sender::~Sender() {}
 
 void Sender::init() {
     configure();
 
-    if (mode_ == REAL) {
-        try {
-            port_.open(port_name_);
-            port_.set_option(boost::asio::serial_port_base::baud_rate(115200));
-            port_.set_option(boost::asio::serial_port_base::character_size(8));
-        } catch (boost::system::system_error error) {
-            std::cout << "[SERIAL COMMUNICATOR ERROR]: " << error.what() << std::endl;
-            {
-                std::lock_guard<std::mutex> lock(mutex_);
-                *running_ = false;
+    switch (execution_mode_) {
+        case REAL:
+            try {
+                serial_writer_ = new furgbol::io::SerialWriter(port_name_, package_size_);
+                serial_writer_->init(
+                    SerialPort::BAUD_500000,
+                    SerialPort::CHAR_SIZE_8,
+                    SerialPort::PARITY_DEFAULT,
+                    SerialPort::STOP_BITS_1,
+                    SerialPort::FLOW_CONTROL_DEFAULT
+                );
+            } catch(std::runtime_error &error) {
+                std::cout << "[SENDER ERROR]: Cannot open serial port." << std::endl;
+                {
+                    std::lock_guard<std::mutex> lock(mutex_);
+                    *running_ = false;
+                }
             }
-        }
-        if (*running_) printConfigurations();
-    } else if (mode_ == SIMULATION) {
-        try {
+            break;
+        case SIMULATION:
             command_sender_ = new vss::CommandSender();
-            switch (team_color_) {
-                case BLUE:
-                    command_sender_->createSocket(vss::TeamType::Blue);
-                    break;
-                case YELLOW:
-                    command_sender_->createSocket(vss::TeamType::Yellow);
-                    break;
+            try {
+                if (team_color_ == BLUE) command_sender_->createSocket(vss::TeamType::Blue);
+                else if (team_color_ == YELLOW) command_sender_->createSocket(vss::TeamType::Yellow);
+            } catch (zmq::error_t &error) {
+                {
+                    std::lock_guard<std::mutex> lock(mutex_);
+                    *running_ = false;
+                }
             }
-        } catch (zmq::error_t& error) {
-            {
-                std::lock_guard<std::mutex> lock(mutex_);
-                *running_ = false;
-            }
-        }
-    }
-    {
-        std::lock_guard<std::mutex> lock(mutex_);
-        *changed_ = true;
+            break;
     }
 
-    exec();
+    if (*running_) {
+        printConfigurations();
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            *changed_ = true;
+        }
+        exec();
+    }
+
+    end();
     {
         std::lock_guard<std::mutex> lock(mutex_);
         *changed_ = true;
@@ -68,164 +72,29 @@ void Sender::init() {
 }
 
 void Sender::configure() {
-    std::cout << std::endl << "[STATUS]: Configuring serial..." << std::endl;
-    std::ifstream _ifstream("config/serial.json");
+    std::cout << "[STATUS]: Configuring sender..." << std::endl;
+
+    std::ifstream ifstream("config/serial.json");
     nlohmann::json json_file;
-    _ifstream >> json_file;
+    ifstream >> json_file;
 
     port_name_ = json_file["port name"];
-    frequency_ = json_file["sending frequency"];
-    period_ = 1/(float)frequency_;
+    package_size_ = json_file["package size"];
 }
 
 void Sender::printConfigurations() {
-    std::cout << "[STATUS]: Serial configuration done!" << std::endl;
-
+    std::cout << "[STATUS]: Sender configuration done!" << std::endl;
+    
     std::cout << "-> Configurations:" << std::endl;
-    std::cout << "Serial port: " << port_name_ << std::endl;
-    std::cout << "Serial sending frequency: " << frequency_ << "hz" << std::endl;
-    std::cout << "Time between serial messages: " << period_ << "s" << std::endl;
-    std::cout << std::endl;
+    std::cout << "\tPort name: " << port_name_ << std::endl;
+    std::cout << "\tPackage size: " << package_size_ << std::endl;
 }
 
-
-void Sender::exec() {
-    std::chrono::system_clock::time_point compair_time = std::chrono::high_resolution_clock::now();
-    bool previous_status = false;
-
-    while (true) {
-        while (*running_) {
-            if (previous_status == false) {
-                previous_status = true;
-                {
-                    std::lock_guard<std::mutex> lock(mutex_);
-                    *changed_ = true;
-                }
-            }
-
-            if ((std::chrono::high_resolution_clock::now() - compair_time) >= sending_frequency_) {
-                switch (which_queue_) {
-                    case GK:
-                        if (*gk_is_running_) {
-                            if (!gk_sending_queue_->empty()) {
-                                {
-                                    std::lock_guard<std::mutex> lock(mutex_);
-                                    send(gk_sending_queue_->front());
-                                }
-                            }
-                            break;
-                        } else if (mode_ == SIMULATION) {
-                            command_.commands.push_back(vss::WheelsCommand(0, 0));
-                        }
-                    case CB:
-                        if (*cb_is_running_) {
-                            if (!cb_sending_queue_->empty()) {
-                                {
-                                    std::lock_guard<std::mutex> lock(mutex_);
-                                    send(cb_sending_queue_->front());
-                                }
-                            }
-                            break;
-                        } else if (mode_ == SIMULATION) {
-                            command_.commands.push_back(vss::WheelsCommand(0, 0));
-                        }
-                    case ST:
-                        if (*st_is_running_) {
-                            if (!st_sending_queue_->empty()) {
-                                {
-                                    std::lock_guard<std::mutex> lock(mutex_);
-                                    send(st_sending_queue_->front());
-                                }
-                            }
-                            break;
-                        } else if (mode_ == SIMULATION) {
-                            command_.commands.push_back(vss::WheelsCommand(0, 0));
-                        }
-                }
-                which_queue_++;
-                if (which_queue_ > ST) {
-                    which_queue_ = GK;
-                    command_sender_->sendCommand(command_);
-                    std::cout << "Sended: {" << std::endl;
-                    std::cout << "\tRobot 1:" << std::endl;
-                    std::cout << "\t\t-> Velocity Right: " << command_.commands[GK].rightVel << std::endl;
-                    std::cout << "\t\t-> Velocity Left: " << command_.commands[GK].leftVel << std::endl;
-                    // std::cout << "\tRobot 2:" << std::endl;
-                    // std::cout << "\t\t-> Velocity Right: " << command_.commands[CB].rightVel << std::endl;
-                    // std::cout << "\t\t-> Velocity Left: " << command_.commands[CB].leftVel << std::endl;
-                    // std::cout << "\tRobot 3:" << std::endl;
-                    // std::cout << "\t\t-> Velocity Right: " << command_.commands[ST].rightVel << std::endl;
-                    // std::cout << "\t\t-> Velocity Left: " << command_.commands[ST].leftVel << std::endl;
-                    // std::cout << "}" << std::endl;
-                    command_.commands.clear();
-                }
-                compair_time = std::chrono::high_resolution_clock::now();
-            }
-        }
-
-        if (!*running_) {
-            end();
-            
-            if (previous_status == true) {
-                previous_status = false;
-                {
-                    std::lock_guard<std::mutex> lock(mutex_);
-                    *changed_ = true;
-                }
-            }
-
-            break;
-        }
+void Sender::send() {
+    switch (execution_mode_) {
+        case REAL:
+            serial_writer_->write
     }
-
-    /*while (running_) {
-        if (!gk_sending_queue_->empty()) {
-            {
-                std::lock_guard<std::mutex> lock(mutex_);
-                buffer = gk_sending_queue_->front();
-            }
- 
-            std::cout << std::endl << "Buffer:" << std::endl;
-            std::cout << "\tRobot ID: " << (int)buffer[operation::ROBOT_ID] << std::endl;
-            std::cout << "\tLinear Velocity: " << (int)buffer[operation::LINEAR_VELOCITY] << std::endl;
-            std::cout << "\tAngular Velocity: " << (int)buffer[operation::ANGULAR_VELOCITY] << std::endl;
-            std::cout << "\tLinear Direction: " << (int)buffer[operation::LINEAR_DIRECTION] << std::endl;
-            std::cout << "\tAngular Direction: " << (int)buffer[operation::ANGULAR_DIRECTION] << std::endl;
-            send(buffer);
-            compair_time = std::chrono::high_resolution_clock::now();
-        }
-    }*/
-}
-
-void Sender::end() {
-    std::cout << "[STATUS]: Closing serial..." << std::endl;
-    try {
-        port_.close();
-    } catch (boost::system::system_error error) {}
-}
-
-void Sender::send(std::vector<unsigned char> buffer) { 
-    if (buffer[ROBOT_ID] >= 128) {
-        switch (mode_) {
-            case REAL:
-                port_.write_some(boost::asio::buffer(buffer, buffer.size()));
-                break;
-            case SIMULATION:
-                linear_velocity_ = (float)buffer[LINEAR_VELOCITY];
-                angular_velocity_ = (float)buffer[ANGULAR_VELOCITY];
-                linear_direction_ = buffer[LINEAR_DIRECTION];
-                angular_direction_ = buffer[ANGULAR_DIRECTION];
-                calculateVelocity();
-                command_.commands.push_back(vss::WheelsCommand(velocity_right_, velocity_left_));
-        }
-    }
-}
-
-void Sender::calculateVelocity() {
-    linear_velocity_ = ((20 * linear_velocity_) / 127.0) * (linear_direction_ - 2);
-    angular_velocity_ = ((20 * angular_velocity_) / 127.0) * (angular_direction_ - 2);
-    velocity_right_ = ((linear_velocity_ / 0.03) + ((angular_velocity_ * 0.04) / 0.03));
-    velocity_left_ = ((linear_velocity_ / 0.03) - ((angular_velocity_ * 0.04) / 0.03));
 }
 
 } // namespace io
